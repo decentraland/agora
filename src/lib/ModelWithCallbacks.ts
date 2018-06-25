5708071460fd604d2d5188216e36832b23867440
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-06-25 16:54:36 +0200

    fix: method order

--- src/lib/ModelWithCallbacks.ts
+++ src/lib/ModelWithCallbacks.ts
@@ -1,18 +1,6 @@
 import { Model, db } from 'decentraland-server'
 
 export class ModelWithCallbacks<T> extends Model<T> {
-  protected static beforeInsert<U>(row: U, _isUpsert: boolean): U {
-    return row
-  }
-
-  protected static beforeUpdate<U>(changes: Partial<U>): Partial<U> {
-    return changes
-  }
-
-  protected static beforeModify<U>(changes: U | Partial<U>) {
-    return changes
-  }
-
   static async insert<U extends db.QueryPart>(
     row: U,
     onConflict: db.OnConflict<U>
@@ -29,5 +17,17 @@ export class ModelWithCallbacks<T> extends Model<T> {
     changes = this.beforeUpdate(changes)
     changes = this.beforeModify(changes)
     return super.update(changes, conditions)
+  }
+
+  protected static beforeInsert<U>(row: U, _isUpsert: boolean): U {
+    return row
+  }
+
+  protected static beforeUpdate<U>(changes: Partial<U>): Partial<U> {
+    return changes
+  }
+
+  protected static beforeModify<U>(changes: U | Partial<U>) {
+    return changes
   }
 }
