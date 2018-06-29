5b3b0771795d6a3f0a6e155adebd4c206e03680c
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-06-29 13:35:05 +0200

    feat: remove submitter and partial results cols

--- webapp/src/components/PollsPage/PollsPage.tsx
+++ webapp/src/components/PollsPage/PollsPage.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
-import { Header, Loader, Table, Blockie, Address, Mana } from 'decentraland-ui'
+import { Header, Loader, Table, Mana } from 'decentraland-ui'
 import { locations } from 'locations'
 import { PollsPageProps } from 'components/PollsPage/types'
 import { t } from 'modules/translation/utils'
@@ -30,9 +30,6 @@ export default class PollsPage extends React.PureComponent<PollsPageProps> {
                   {t('polls_page.table.title')}
                 </Table.HeaderCell>
                 <Table.HeaderCell>
-                  {t('polls_page.table.submitter')}
-                </Table.HeaderCell>
-                <Table.HeaderCell>
                   {t('polls_page.table.balance')}
                 </Table.HeaderCell>
                 <Table.HeaderCell>
@@ -46,11 +43,6 @@ export default class PollsPage extends React.PureComponent<PollsPageProps> {
                 <Table.Row key={index}>
                   <Table.Cell>
                     <Link to={locations.pollDetail(poll.id)}>{poll.title}</Link>
-                  </Table.Cell>
-                  <Table.Cell>
-                    <Blockie scale={3} seed={poll.submitter}>
-                      &nbsp;<Address value={poll.submitter} />
-                    </Blockie>
                   </Table.Cell>
                   <Table.Cell>
                     {poll.token.symbol === 'MANA' ? (
