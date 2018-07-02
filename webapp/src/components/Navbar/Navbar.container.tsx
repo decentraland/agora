ea236b32712a3c4b078e2b94b5d247bfe0e777fa
Juan Cazala <juan@decentraland.org>
2018-07-02 11:58:35 -0400

    chore: linted & added translations

--- webapp/src/components/Navbar/Navbar.container.tsx
+++ webapp/src/components/Navbar/Navbar.container.tsx
@@ -2,14 +2,13 @@ import * as React from 'react'
 import { connect } from 'react-redux'
 import { push, goBack, RouterAction } from 'react-router-redux'
 import { env } from 'decentraland-commons'
-import { Navbar } from 'decentraland-ui'
+import { Navbar, Menu } from 'decentraland-ui'
 import { locations } from 'locations'
 import { RootState, RootDispatch } from 'types'
 import { Wallet } from 'modules/wallet/types'
 import { getWallet, isConnected, isConnecting } from 'modules/wallet/selectors'
 import { isModalPage } from 'modules/location/selectors'
 import { t } from 'modules/translation/utils'
-import { Menu } from 'decentraland-ui'
 
 const mapState = (state: RootState): any => {
   const wallet = getWallet(state) as Wallet
