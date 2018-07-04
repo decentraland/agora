354e3381f6a1d74d30843c6e3bed386aa0545f7c
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-07-04 12:46:14 +0200

    feat: sign nonce on the server receipt

--- src/Receipt/Receipt.model.ts
+++ src/Receipt/Receipt.model.ts
@@ -18,26 +18,39 @@ export class Receipt extends ModelWithCallbacks<ReceiptAttributes> {
     return this.deleteNonce(row)
   }
 
+  static canSign() {
+    const serverKey = env.get('SERVER_SIGNING_KEY', '')
+    if (!serverKey) {
+      console.warn(
+        '[WARN] SERVER_SIGNING_KEY env var is undefined. Receipts are disabled'
+      )
+    }
+    return !!serverKey
+  }
+
   static async createFromVote(
     vote: CastVoteOption
   ): Promise<ReceiptAttributes | undefined> {
-    const receipt = new Receipt()
-    const signResult = receipt.sign(vote)
-
-    if (signResult) {
-      const attributes: ReceiptAttributes = {
+    if (Receipt.canSign()) {
+      const receipt = new Receipt({
         account_message: vote.message,
         account_signature: vote.signature,
         account_address: vote.account_address,
-        server_message: signResult.message,
-        server_signature: signResult.signature,
+        server_message: '',
+        server_signature: '',
         option_value: vote.option.value,
-        vote_id: vote.id
-      }
-
-      receipt.assign(attributes)
-
+        vote_id: vote.id,
+        nonce: -1
+      })
       await receipt.create()
+      await receipt.retreive()
+
+      const signResult = await receipt.sign(vote, receipt.get('nonce'))
+      receipt.assign({
+        server_message: signResult.message,
+        server_signature: signResult.signature
+      })
+      await receipt.update()
 
       return receipt.getAll()
     }
@@ -49,29 +62,18 @@ export class Receipt extends ModelWithCallbacks<ReceiptAttributes> {
     return attributes
   }
 
-  async sign(vote: CastVoteOption): Promise<Signature | null> {
+  async sign(vote: CastVoteOption, nonce: number): Promise<Signature> {
     const serverKey = env.get('SERVER_SIGNING_KEY', '')
-    if (!serverKey) {
-      console.warn(
-        '[WARN] SERVER_SIGNING_KEY env var is undefined. Receipts are disabled'
-      )
-      return null
-    }
+    const hash = eth.utils.sha3(vote.message, 256).toString('hex')
 
     const message = eth.utils.toHex(
       `Decentraland Vote Receipt:\nMessage from address: ${
         vote.account_address
-      }, with hash ${
-        eth.utils.sha3(vote.message, 256).toString('hex')
-      } and signature ${
+      }, with hash ${hash} and signature ${
         vote.signature
-      } was received.\n\nThe vote to cast is: ${
-        vote.option.value
-      } on poll ${vote.pollId}.\n\nDate: ${
-        new Date()
-      }\nServer nonce: ${
-        await Receipt.count()
-      }`
+      } was received.\n\nThe vote to cast is: ${vote.option.value} on poll ${
+        vote.poll_id
+      }.\n\nDate: ${Date.now()}\nServer nonce: ${nonce}`
     )
     const signature = eth.utils.localSign(message, serverKey)
 
