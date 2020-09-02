be8a0fcbeefa040d06c25da1097ea744cbd6d01f
Fede <i@2fd.me>
2020-09-02 13:25:40 -0300

    feat: support markdown

--- webapp/src/components/PollDetailPage/PollRanking/PollRanking.tsx
+++ webapp/src/components/PollDetailPage/PollRanking/PollRanking.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react'
+import * as Markdown from 'react-markdown'
 import { Table, Pagination, PaginationProps } from 'decentraland-ui'
 import Token from 'components/Token'
 import './PollRanking.css'
@@ -15,19 +16,32 @@ export default class PollRanking extends React.Component<Props, State> {
     return Math.ceil(this.props.results.length / VOTES_PER_PAGE)
   }
 
+  getResults() {
+    const { activePage } = this.state
+    const offset = VOTES_PER_PAGE * (activePage - 1)
+
+    return this.props.results
+      .sort((resultA, resultB) => {
+        return (
+          resultB.percentage - resultA.percentage ||
+          resultA.option.value.localeCompare(resultB.option.value)
+        )
+      })
+      .slice(offset, offset + VOTES_PER_PAGE)
+  }
+
   handlePageChange = (_: React.MouseEvent<any>, props: PaginationProps) => {
-    const activePage = Math.max(1, Math.min(props.totalPages as number, props.activePage as number))
+    const activePage = Math.max(
+      1,
+      Math.min(props.totalPages as number, props.activePage as number)
+    )
     this.setState({ activePage })
   }
 
   render() {
     const { activePage } = this.state
     const offset = VOTES_PER_PAGE * (activePage - 1)
-    const results = this.props.results
-      .sort((resultA, resultB) => {
-        return resultB.percentage - resultA.percentage
-      })
-      .slice(offset, offset + VOTES_PER_PAGE)
+    const results = this.getResults()
 
     return (
       <div className="PollRanking">
@@ -43,48 +57,39 @@ export default class PollRanking extends React.Component<Props, State> {
 
           <Table.Body>
             {results.map(({ option, votes, percentage, token }, index) => {
-              return <Table.Row key={index}>
-                <Table.Cell className="ranking">{offset + index + 1}</Table.Cell>
-                <Table.Cell>{option.value}</Table.Cell>
-                <Table.Cell>
-                  <Token token={token!} amount={votes} cell />
-                </Table.Cell>
-                <Table.Cell>{percentage} % </Table.Cell>
-              </Table.Row>
-            })}
-            {/* {rows.map(({ poll, weight, voters, status, outcome }, index) => {
-              const isClosed = status === 'Closed'
-              const linkClass = isClosed ? 'link-closed' : null
-              const noLinkClass = isClosed ? 'closed' : null
               return (
                 <Table.Row key={index}>
+                  <Table.Cell className="ranking">
+                    {offset + index + 1}
+                  </Table.Cell>
                   <Table.Cell>
-                    <a className={linkClass} href="#">
-                      {poll}
-                    </a>
+                    <Markdown
+                      source={option.value}
+                      allowedTypes={['text', 'paragraph', 'link']}
+                    />
                   </Table.Cell>
-                  <Table.Cell className={noLinkClass}>
-                    <Mana inline />
-                    {weight}
+                  <Table.Cell>
+                    <Token token={token!} amount={votes} cell />
                   </Table.Cell>
-                  <Table.Cell className={noLinkClass}>{voters}</Table.Cell>
-                  <Table.Cell className={noLinkClass}>{status}</Table.Cell>
-                  <Table.Cell className={noLinkClass}>{outcome}</Table.Cell>
+                  <Table.Cell>{percentage} %</Table.Cell>
                 </Table.Row>
               )
-            })} */}
+            })}
           </Table.Body>
         </Table>
-        {this.props.results.length > VOTES_PER_PAGE && <div className="RankingPagination">
-          <Pagination
-            activePage={this.state.activePage}
-            totalPages={this.getTotalPages()}
-            firstItem={null}
-            lastItem={null}
-            nextItem={null}
-            prevItem={null}
-            onPageChange={this.handlePageChange} />
-        </div>}
+        {this.props.results.length > VOTES_PER_PAGE && (
+          <div className="RankingPagination">
+            <Pagination
+              activePage={this.state.activePage}
+              totalPages={this.getTotalPages()}
+              firstItem={null}
+              lastItem={null}
+              nextItem={null}
+              prevItem={null}
+              onPageChange={this.handlePageChange}
+            />
+          </div>
+        )}
       </div>
     )
   }
