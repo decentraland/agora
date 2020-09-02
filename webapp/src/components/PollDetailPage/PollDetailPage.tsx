89a3bc817b0dba65108f0e6c0858c33c9aa165ee
Fede Ramirez <i@2fd.me>
2020-09-02 14:39:13 -0300

    feat/ranking (#150)
    * feat: use ranking table
    
    * feat: add ranking pagination
    
    * chore: fix circle database
    
    * lint: PollDetailPage

--- webapp/src/components/PollDetailPage/PollDetailPage.tsx
+++ webapp/src/components/PollDetailPage/PollDetailPage.tsx
@@ -133,190 +133,195 @@ export default class PollDetailPage extends React.PureComponent<Props, State> {
         {isLoading || !poll ? (
           <Loader active size="massive" />
         ) : (
-            <>
-              <Header size="large">{poll.title}</Header>
-              {poll.description ? (
-                <div className="description-wrapper">
-                  <Header sub className="description-header">
-                    {t('global.description')}
-                  </Header>
-                  <ReactMarkdown
-                    className="description"
-                    source={poll.description}
-                  />
-                </div>
-              ) : null}
-              <div className="stats">
-                {isDistrictToken(poll.token) ? (
-                  <Stats
-                    title={t('global.your_contributions')}
-                    className="voting-with"
-                  >
-                    <Header>{getBalanceInPoll(wallet, poll) || 0}</Header>
-                  </Stats>
-                ) : (
-                    <Stats title={t('poll_detail_page.stats.token')}>
-                      <Token token={poll.token} />
-                    </Stats>
-                  )}
-                <Stats title={t('global.weight')}>
-                  <Token token={poll.token} amount={poll.balance} />
-                </Stats>
-                <Stats title={t('global.votes')}>
-                  <Header>{poll.votes.length}</Header>
-                </Stats>
-                {isFinished(poll) ? (
-                  <Stats title={t('poll_detail_page.stats.finished')}>
-                    <Header>{formatDate(poll.closes_at)}</Header>
-                  </Stats>
-                ) : (
-                    <Stats title={t('poll_detail_page.stats.time_left')}>
-                      <Header>{distanceInWordsToNow(poll.closes_at, false)}</Header>
-                    </Stats>
-                  )}
+          <>
+            <Header size="large">{poll.title}</Header>
+            {poll.description ? (
+              <div className="description-wrapper">
+                <Header sub className="description-header">
+                  {t('global.description')}
+                </Header>
+                <ReactMarkdown
+                  className="description"
+                  source={poll.description}
+                />
               </div>
+            ) : null}
+            <div className="stats">
+              {isDistrictToken(poll.token) ? (
+                <Stats
+                  title={t('global.your_contributions')}
+                  className="voting-with"
+                >
+                  <Header>{getBalanceInPoll(wallet, poll) || 0}</Header>
+                </Stats>
+              ) : (
+                <Stats title={t('poll_detail_page.stats.token')}>
+                  <Token token={poll.token} />
+                </Stats>
+              )}
+              <Stats title={t('global.weight')}>
+                <Token token={poll.token} amount={poll.balance} />
+              </Stats>
+              <Stats title={t('global.votes')}>
+                <Header>{poll.votes.length}</Header>
+              </Stats>
+              {isFinished(poll) ? (
+                <Stats title={t('poll_detail_page.stats.finished')}>
+                  <Header>{formatDate(poll.closes_at)}</Header>
+                </Stats>
+              ) : (
+                <Stats title={t('poll_detail_page.stats.time_left')}>
+                  <Header>{distanceInWordsToNow(poll.closes_at, false)}</Header>
+                </Stats>
+              )}
+            </div>
 
-              <Responsive
-                minWidth={(Responsive.onlyTablet.minWidth as number) + 1}
-              >
-                <>
-                  <div className="row main">
-                    <div className="progress">
-                      {currentResults.length <= 5 && <PollProgress results={currentResults} />}
-                      {currentResults.length > 5 && <PollRanking results={currentResults} />}
-                    </div>
-
-                    <CastYourVote poll={poll} isConnected={isConnected} />
+            <Responsive
+              minWidth={(Responsive.onlyTablet.minWidth as number) + 1}
+            >
+              <>
+                <div className="row main">
+                  <div className="progress">
+                    {currentResults.length <= 5 && (
+                      <PollProgress results={currentResults} />
+                    )}
+                    {currentResults.length > 5 && (
+                      <PollRanking results={currentResults} />
+                    )}
                   </div>
 
-                  <div className="row sub">
-                    <div className="options">
-                      {currentResults.length <= 5 && currentResults.map((result, index) => (
+                  <CastYourVote poll={poll} isConnected={isConnected} />
+                </div>
+
+                <div className="row sub">
+                  <div className="options">
+                    {currentResults.length <= 5 &&
+                      currentResults.map((result, index) => (
                         <OptionOrb position={index} key={result.option.id}>
                           {result.option.value}
                         </OptionOrb>
                       ))}
-                    </div>
-                    {isConnected ? (
-                      <YourVote vote={currentVote} poll={poll} />
-                    ) : (
-                        <p className="sign-in-message">
-                          {isFinished(poll) ? (
-                            <T id="poll_detail_page.poll_closed" />
-                          ) : (
-                              <T
-                                id="poll_detail_page.sign_in_message"
-                                values={{
-                                  sign_in_link: (
-                                    <Link to={locations.signIn()}>
-                                      {t('poll_detail_page.sign_in_link')}
-                                    </Link>
-                                  )
-                                }}
-                              />
-                            )}
-                        </p>
-                      )}
                   </div>
-                </>
-              </Responsive>
-
-              <Responsive maxWidth={Responsive.onlyTablet.minWidth}>
-                <div className="results">
-                  {currentResults.map((result, index) => (
-                    <OptionBar
-                      position={index}
-                      percentage={result.percentage}
-                      key={result.option.id}
-                    >
-                      {result.option.value}
-                    </OptionBar>
-                  ))}
+                  {isConnected ? (
+                    <YourVote vote={currentVote} poll={poll} />
+                  ) : (
+                    <p className="sign-in-message">
+                      {isFinished(poll) ? (
+                        <T id="poll_detail_page.poll_closed" />
+                      ) : (
+                        <T
+                          id="poll_detail_page.sign_in_message"
+                          values={{
+                            sign_in_link: (
+                              <Link to={locations.signIn()}>
+                                {t('poll_detail_page.sign_in_link')}
+                              </Link>
+                            )
+                          }}
+                        />
+                      )}
+                    </p>
+                  )}
                 </div>
-                <CastYourVote poll={poll} isConnected={isConnected} />
-                <YourVote vote={currentVote} poll={poll} />
-              </Responsive>
+              </>
+            </Responsive>
 
-              {noVotes ? null : (
-                <div className="votes">
-                  <Header sub>{t('poll_detail_page.votes')}</Header>
-                  <Table basic="very">
-                    <Table.Header>
-                      <Table.Row>
-                        <Table.HeaderCell>
-                          {t('poll_detail_page.when')}
-                        </Table.HeaderCell>
-                        <Table.HeaderCell>
-                          {t('poll_detail_page.address')}
-                        </Table.HeaderCell>
-                        <Table.HeaderCell>{t('global.weight')}</Table.HeaderCell>
-                        <Table.HeaderCell>
-                          {t('poll_detail_page.vote')}
-                        </Table.HeaderCell>
-                      </Table.Row>
-                    </Table.Header>
+            <Responsive maxWidth={Responsive.onlyTablet.minWidth}>
+              <div className="results">
+                {currentResults.map((result, index) => (
+                  <OptionBar
+                    position={index}
+                    percentage={result.percentage}
+                    key={result.option.id}
+                  >
+                    {result.option.value}
+                  </OptionBar>
+                ))}
+              </div>
+              <CastYourVote poll={poll} isConnected={isConnected} />
+              <YourVote vote={currentVote} poll={poll} />
+            </Responsive>
 
-                    <Table.Body>
-                      {poll.votes
-                        .slice(pageOffset, pageOffset + VOTES_PER_PAGE)
-                        .map((vote, index) => (
-                          <Table.Row key={vote.id + index}>
-                            <Table.Cell title={formatDateTime(vote.timestamp)}>
-                              <span className="mobile-header">
-                                {t('poll_detail_page.when')}
+            {noVotes ? null : (
+              <div className="votes">
+                <Header sub>{t('poll_detail_page.votes')}</Header>
+                <Table basic="very">
+                  <Table.Header>
+                    <Table.Row>
+                      <Table.HeaderCell>
+                        {t('poll_detail_page.when')}
+                      </Table.HeaderCell>
+                      <Table.HeaderCell>
+                        {t('poll_detail_page.address')}
+                      </Table.HeaderCell>
+                      <Table.HeaderCell>{t('global.weight')}</Table.HeaderCell>
+                      <Table.HeaderCell>
+                        {t('poll_detail_page.vote')}
+                      </Table.HeaderCell>
+                    </Table.Row>
+                  </Table.Header>
+
+                  <Table.Body>
+                    {poll.votes
+                      .slice(pageOffset, pageOffset + VOTES_PER_PAGE)
+                      .map((vote, index) => (
+                        <Table.Row key={vote.id + index}>
+                          <Table.Cell title={formatDateTime(vote.timestamp)}>
+                            <span className="mobile-header">
+                              {t('poll_detail_page.when')}
                               :&nbsp;
                             </span>
-                              {formatDate(vote.timestamp)}
-                            </Table.Cell>
-                            <Table.Cell>
-                              <span className="mobile-header">
-                                {t('poll_detail_page.address')}
+                            {formatDate(vote.timestamp)}
+                          </Table.Cell>
+                          <Table.Cell>
+                            <span className="mobile-header">
+                              {t('poll_detail_page.address')}
                               :&nbsp;
                             </span>
-                              <Blockie scale={3} seed={vote.account_address}>
-                                &nbsp;
+                            <Blockie scale={3} seed={vote.account_address}>
+                              &nbsp;
                               <Address value={vote.account_address} />
-                              </Blockie>
-                            </Table.Cell>
-                            <Table.Cell>
-                              <span className="mobile-header">
-                                {t('global.weight')}
+                            </Blockie>
+                          </Table.Cell>
+                          <Table.Cell>
+                            <span className="mobile-header">
+                              {t('global.weight')}
                               :&nbsp;
                             </span>
-                              <Token
-                                token={poll.token}
-                                amount={vote.account_balance}
-                                cell
-                              />
-                            </Table.Cell>
-                            <Table.Cell>
-                              <span className="mobile-header">
-                                {t('poll_detail_page.vote')}
+                            <Token
+                              token={poll.token}
+                              amount={vote.account_balance}
+                              cell
+                            />
+                          </Table.Cell>
+                          <Table.Cell>
+                            <span className="mobile-header">
+                              {t('poll_detail_page.vote')}
                               :&nbsp;
                             </span>
-                              {getVoteOptionValue(poll.options, vote)}
-                            </Table.Cell>
-                          </Table.Row>
-                        ))}
-                    </Table.Body>
-                  </Table>
-                  {totalPages > 1 ? (
-                    <div className="votes-pagination">
-                      <Pagination
-                        activePage={this.state.activePage}
-                        totalPages={totalPages}
-                        firstItem={null}
-                        lastItem={null}
-                        nextItem={null}
-                        prevItem={null}
-                        onPageChange={this.handlePageChange}
-                      />
-                    </div>
-                  ) : null}
-                </div>
-              )}
-            </>
-          )}
+                            {getVoteOptionValue(poll.options, vote)}
+                          </Table.Cell>
+                        </Table.Row>
+                      ))}
+                  </Table.Body>
+                </Table>
+                {totalPages > 1 ? (
+                  <div className="votes-pagination">
+                    <Pagination
+                      activePage={this.state.activePage}
+                      totalPages={totalPages}
+                      firstItem={null}
+                      lastItem={null}
+                      nextItem={null}
+                      prevItem={null}
+                      onPageChange={this.handlePageChange}
+                    />
+                  </div>
+                ) : null}
+              </div>
+            )}
+          </>
+        )}
       </div>
     )
   }
